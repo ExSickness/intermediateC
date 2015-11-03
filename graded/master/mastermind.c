fd71fd0b5b702c1d6ba34b77f672ae42dabfd4f8
asamuels <american_cyko@yahoo.com>
2015-11-03 14:07:45 -0800

    Mastermind really working this time. loop is going twice

--- graded/master/mastermind.c
+++ graded/master/mastermind.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX 5
 
@@ -18,49 +19,51 @@ int game()
 {
 	int target[MAX];
 	char guess[MAX];
+	int count = 0,red=0,white=0;
 
 	printf("Target is: ");
+	srand(time(NULL));
 	for (int i = 0; i < 4; i++)
 	{
+		
 		target[i] = rand() % 10;
 		printf("%d",target[i]);
 	}
 	printf("\n");
 
-	userInput(guess);
+	while(red != 4)
+	{
+		red=0;
+		white=0;
+		userInput(guess);
 
-	int red=0,white=0;
-	// Red loop
-	for(int i = 0; i < 4; i++)
-	{
 		
-		if(target[i] == guess[i])
+		// Loop
+		for(int i = 0; i < 4;i++)       // 0,1,2,3
 		{
-			printf("Red found!\n");
-			red +=1;
-		}
-		
-	}
-	// White loop
-	for(int i = 0; i < 4;i++)       // 0,1,2,3
-	{
-		if(target[i] == guess[i])
-		{
-			continue;
-		}
-		for(int p = 3; p >= 0; p--) // 3,2,1,0
-		{
-			if(guess[i] == target[p])
+			if(target[i] == guess[i])
 			{
-				white +=1;
-				printf("White found!\n");
+				printf("Red found!\n");
+				red +=1;
+				printf("Skippping. i : %d ",i);
+				continue;
+			}
+			for(int p = 3; p >= 0; p--) // 3,2,1,0
+			{
+				//printf("i: %d    p: %d\n",i,p);
+				if(guess[i] == target[p])
+				{
+					white +=1;
+					printf("White found!\n");
+					break;
+				}
 			}
 		}
+
+		printf("%d red, %d white, guess count: %d\n",red,white,count);
+		count +=1;
 	}
-
-	printf("%d red, %d white\n",red,white);
-
-
+	printf("You've won in %d guesses.",count);
 
 
 	return(0);
