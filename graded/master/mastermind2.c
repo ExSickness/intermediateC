93b783135b9e2f83e87eb22a61ab67a699dc3a83
asamuels <american_cyko@yahoo.com>
2015-11-05 12:10:14 -0800

    100% sure mastermind works this time.

--- graded/master/mastermind2.c
+++ graded/master/mastermind2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX 5
+#define MAX 6
 
 void userInput(char* input);
 int game();
@@ -25,24 +25,29 @@ int game()
 	srand(time(NULL));
 	for (int i = 0; i < 4; i++)
 	{
-		
 		target[i] = rand() % 10;
-		//printf("%d",target[i]);
+		printf("%d",target[i]);
 	}
 	printf("\n");
 
-	target[0] = 5;
-	target[1] = 8;
-	target[2] = 8;
-	target[3] = 8;
+/*
+	target[0] = 7;
+	target[1] = 3;
+	target[2] = 4;
+	target[3] = 3;
 
-	
+	printf("%d",target[0]);
+	printf("%d",target[1]);
+	printf("%d",target[2]);
+	printf("%d\n",target[3]);
+*/
 
 	while(red != 4)
 	{
 		red=0;
 		white=0;
 		int skip[4] = {0};
+		int whiteSkip[4] = {0};
 		userInput(guess);
 
 		// Red Loop, sets indexes to skip
@@ -52,6 +57,7 @@ int game()
 			{
 				red +=1;
 				skip[i] = 1;
+				whiteSkip[i] = 1;
 			}
 		}
 		printf("%d",skip[0]);
@@ -60,33 +66,38 @@ int game()
 		printf("%d\n",skip[3]);
 		for(int p = 0; p <4; p++) // 3,2,1,0
 		{
+			//printf("\nBLAHBLAH P IS: %d\n",p);
 			if(skip[p])
 				{
+					//printf("Continuing. p is: %d\n",p);
+					whiteSkip[p] = 1;
 					continue;
 				}
 
 			for(int x = 0;x<4; x++)
 			{
-				if(skip[x])
+				//printf("XXXXXX: %d\n",x);
+				if((whiteSkip[x]) || (skip[p]))
 				{
+					//printf("Skipping loop index %d\n",x);
 					continue;
 				}
-				if(guess[p] == target[x])
+				else if(guess[p] == target[x])
 				{
 					white +=1;
-					printf("Found white.\n");
-					skip[x] = 1;
+					//printf("Found white. %d and %d\n",guess[p],target[x]);
+					whiteSkip[x] = 1;
 					break;
 				}
 			}
 		}
 		count +=1;
 
-		printf("%d - %d red, %d white\n",count,red,white);
+		printf("%d red, %d white\n",red,white);
+		printf("=======================================\n");
 		
 	}
 	printf("You've won in %d guesses.\n",count);
-
 
 	return(0);
 }
@@ -96,12 +107,10 @@ int game()
 void userInput(char* user)
 {
 	printf("Guess a number: ");
-	char buffer[MAX];
 	fgets(user,MAX,stdin);
 
 	for(int i = 0; i < 4; i++)
 	{
 		user[i] = user[i] - 48;
 	}
-	fgets(buffer,MAX,stdin);
 }
